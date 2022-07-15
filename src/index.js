f1bc4daa2bbed34169478f07e19a8deb92759e6b
Anuar <anuarnurshinov@gmail.com>
2022-07-15 13:03:21 +0300

    Основа

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import modal from './modules/modal'
+import sendForm from './modules/sendForm'
+import validator from './modules/validator'
+import scroller from './modules/scroller'
+
+modal()
+sendForm()
+validator()
+scroller()
