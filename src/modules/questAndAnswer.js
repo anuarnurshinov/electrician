b78644e8a0e348e34e0fa7fae27c55d9b5ddef55
Anuar <anuarnurshinov@gmail.com>
2022-07-17 11:23:08 +0300

    Обновлен Аккордеон

--- src/modules/questAndAnswer.js
+++ src/modules/questAndAnswer.js
@@ -1,18 +1,28 @@
 const questAndAnswer = () => {
     const accordeon = document.querySelector('.accordeon')
-
+    const elementsList = accordeon.querySelectorAll('.element-content')
+    const elements = accordeon.querySelectorAll('.element')
+    
 
     accordeon.addEventListener('click', (e) => {
-        const content = e.target.closest('.element').querySelector('.element-content')
-        const element = e.target.closest('.accordeon').querySelector('.element')
-
+        if(e.target.closest('.element')){
+            const content = e.target.closest('.element').querySelector('.element-content')
 
         if (content.style.getPropertyValue("display") === "block") {
             content.style.display = "none"
         } else {
+            elements.forEach(el => {
+                if(el.classList.contains('active')){
+                    el.classList.toggle('active')
+                    el.querySelector('.element-content').style.display ="none"
+                }
+                
+            });
             content.style.display = "block"
+
         }
         e.target.closest('.element').classList.toggle('active')
+        }
     })
 }
 export default questAndAnswer
